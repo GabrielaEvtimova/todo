5dfea74b91140d086d2ee517ff15f592753842ed
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-18 16:19:36 +0300

    Create AddTodo component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from "react";
+import AddTodo from "./components/AddTodo/AddTodo";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [showAddTodo, setShowAddTodo] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-custom-gradient w-screen h-screen select-none">
+      <Header setShowAddTodo={setShowAddTodo} showAddTodo={showAddTodo} />
+      <div className=" flex justify-center items-center">
+        <AddTodo showAddTodo={showAddTodo} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import Select from "react-select";
+import { useState } from "react";
+import { reactSelectStyles } from "../../services/react-select-styles";
+
+const options = [
+  { value: "high", label: "High" },
+  { value: "medium", label: "Medium" },
+  { value: "low", label: "Low" },
+];
+
+export default function AddTodo({ showAddTodo }) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  return (
+    showAddTodo && (
+      <div className="grid mt-16 justify-center items-center shadow-black shadow-lg w-[530px] md:w-[720px] py-4 pb-8">
+        <h1 className="flex items-center py-4 text-4xl w-full justify-center rounded-md text-white mb-4 shadow-inner shadow-neutral-900 mt-8">
+          Create New Task
+        </h1>
+
+        <form className="w-full" onSubmit={() => {}}>
+          <input
+            type="text"
+            placeholder="Add task here..."
+            className="w-[500px] md:w-[560px] py-2 my-4 border-none outline-none rounded-md pl-2 placeholder:text-neutral-500"
+          />
+          <Select
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+            value={selectedOption}
+            placeholder="Select a priority..."
+            styles={reactSelectStyles}
+          />
+
+          <input
+            type="date"
+            className="py-2 my-4 rounded-md px-2 outline-none w-full text-neutral-800 text-[11pt]"
+          />
+          <textarea
+            name="textArea"
+            id="textArea"
+            cols="30"
+            rows="5"
+            className="w-full outline-none px-2 py-2 rounded-md  placeholder:text-neutral-500"
+            placeholder="Provide details here..."
+          ></textarea>
+
+          <button className=" bg-[#ffea00] py-2 my-2 rounded-md mb-2 w-full font-semibold mt-8">
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  );
+}
+
+AddTodo.propTypes = {
+  showAddTodo: PropTypes.bool,
+};
