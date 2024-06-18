959b31bcfdedaa926e13a83826e33cbb02b8d228
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-18 15:48:23 +0300

    Install Express and MongoDB and create a server file

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+server.listen(3000, () => {
+    console.info("Server is listening at http://localhost:3000")
+})
