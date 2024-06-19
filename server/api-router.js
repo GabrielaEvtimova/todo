d50bacaf2a28483741440c86bb3c5f124183a8a3
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-20 01:56:14 +0300

    Add Delete Todo API Endpoint

--- server/api-router.js
+++ server/api-router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectClient } from "./db.js";
 import cors from "cors";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 router.use(cors());
@@ -25,9 +26,10 @@ router.get("/todos", async (req, res) => {
   res.send({ todos });
 });
 
+// Add Todo
 router.post("/todos", async (req, res) => {
   const client = await connectClient();
- 
+
   const { todoTitle, id, label, dueDate, description } = req.body;
 
   const doc = await client.collection("todos").insertOne({
@@ -44,6 +46,20 @@ router.post("/todos", async (req, res) => {
     .findOne({ _id: doc.insertedId });
 
   res.send({ todo });
+});
+
+// Delete Todo
+
+router.delete("/todos/:id", async (res, req) => {
+  const id = req.req.params.id;
+  
+  const client = await connectClient();
+  const todo = await client
+    .collection("todos")
+    .deleteOne({_id: new ObjectId(id)});
+
+   res.send("Successful")
+
 });
 
 export default router;
