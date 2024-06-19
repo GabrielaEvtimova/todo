b9681553aa65b05d34bdbc2cd4b3736c2ecd957b
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-19 22:29:48 +0300

    Cosmetic changes in Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     
   return (
-    <div className="flex justify-center items-center pt-12 w-full pb-9 bottom-0 bg-[#0e1645] text-neutral-400 fixed">
+    <div className="flex justify-center items-center pt-6 w-full pb-6 bottom-0 bg-[#0e1645] text-neutral-400 fixed shadow-inner shadow-black">
       © 2024 TodoApp
     </div>
   );
